539f43b2563e461ae4d19accc021ce19f4e69435
Ramkos1112 <ramkos1112@gmail.com>
2025-03-05 11:50:26 +0200

    task 5

--- TS/Task 5/script.js
+++ TS/Task 5/script.js
@@ -7,14 +7,15 @@ Pvz.:
   "scissors", "rock" --> "Player 2 won!"
   "paper", "paper" --> "Draw!"
 -------------------------------------------------------------------------- */
-
-const game = (player1: "scissors" | "paper" | "rock", player2: "scissors" | "paper" | "rock"): string => {
-  if(player1 === player2){
-    return "Draw!"
-  }else if((player1 === "scissors" && player2 === "paper") || (player1 === "paper" && player2 === "rock") || (player1 === "rock" && player2 === "scissors")){
-    return "Player 1 won!"
-  }else{
-    return "Player 2 won!"
-}};
-
-console.log(game("scissors", "paper"))
+var game = function (player1, player2) {
+    if (player1 === player2) {
+        return "Draw!";
+    }
+    else if ((player1 === "scissors" && player2 === "paper") || (player1 === "paper" && player2 === "rock") || (player1 === "rock" && player2 === "scissors")) {
+        return "Player 1 won!";
+    }
+    else {
+        return "Player 2 won!";
+    }
+};
+console.log(game("scissors", "paper"));
